42ebea789e305a8d91d85547dc5dd737e655083b
anastasia <anastasiamhln@gmail.com>
2023-01-20 14:09:03 +0300

    комит

--- src/main/java/lesson3/TestOne.java
+++ src/main/java/lesson3/TestOne.java
@@ -1,11 +1,16 @@
 package lesson3;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+import java.util.concurrent.TimeUnit;
+
+//Тест-кейс № 1. Проверка авторизации
 public class TestOne {
     public static void main(String[] args) {
 
@@ -15,18 +20,43 @@ public class TestOne {
         options.addArguments("start-maximized");
 
         WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
         driver.get("https://www.lamoda.ru/");
-
-        WebElement auth = driver.findElement();
-
-<a class="_root_clp6c_2 _secondaryLabel_clp6c_13 _link_1gjtp_11 _link_1gjtp_11" role="button">Войти</a>
-        //*[@id="vue-rroot"]/div/heade/div[2]/div/a[1]
-        /html/body/div[1]/div/header/div[2]/div/a[1]
+        WebElement auth = driver.findElement(By.xpath("//*[text()[contains(.,'Войти')]]"));
+        auth.click();
 
 
+        WebElement login = driver.findElement(By.name("login"));
+        login.click();
+        login.sendKeys("nastusha665@mail.ru");
 
-        div._root_1gjtp_2> a[role = "button"]
+        WebElement password = driver.findElement(By.cssSelector("input[name=password]"));
+        password.click();
+        password.sendKeys("1234Book@");
+
+        WebElement singIn = driver.findElement(By.cssSelector("button[aria-label=Войти]"));
+        singIn.click();
+
+
+        try {
+            WebElement allert = driver.findElement(By.xpath("//*[text()[contains(.,'Супер!')]]"));
+            allert.click();
+        }
+        catch (NoSuchElementException exception) {
+            System.out.println("Элемент не найден");
+        }
+
+
+        WebElement profile = driver.findElement(By.cssSelector("span[role=link]>a[role=link]"));
+        profile.click();
+
+        driver.quit();
+
+
+
+
+
 
     }
 }
